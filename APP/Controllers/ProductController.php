3a2ff54114ceb95948559067a1bdf9cc0f76c2ed
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-02 00:27:28 +0300

    Enhance ProductController with detailed methods for product management
    - Add index method to fetch and display all products with a title
    - Implement add method to load the view for adding new products
    - Create edit method to retrieve and display specific product data for editing, including error handling if the product is not found

--- APP/Controllers/ProductController.php
+++ APP/Controllers/ProductController.php
@@ -2,10 +2,18 @@
 
 class ProductController
 {
+    // Displays a list of all products
     public function index(): void
     {
-        $db = new product();
-        $data['products'] = $db->getAllProduct();
-        View::load('Product/index' , $data);
+        $db = new product(); // Create a new instance of the product model
+        $data['products'] = $db->getAllProduct(); // Fetch all products from the database
+        $data['title'] = "Products"; // Set the title for the view
+        View::load('Product/index', $data); // Load the index view with product data
+    }
+
+    // Loads the view for adding a new product
+    public function add(): void
+    {
+        View::load('Product/add'); // Load the add product view
     }
 }

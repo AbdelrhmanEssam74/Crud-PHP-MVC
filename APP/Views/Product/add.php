85be8f0364eb7f222e1fa33e01429c0c952af536
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-02 00:56:23 +0300

    Add success message display and standardize input field names in add.php
    - Implement success message alert after form submission.
    - Change input field names for price, description, and quantity to lowercase for consistency.

--- APP/Views/Product/add.php
+++ APP/Views/Product/add.php
@@ -3,25 +3,34 @@
     <div class="container">
         <div class="row">
             <div class="col-10 mx-auto p-4 border mb-5">
+                <?php
+                if (!empty($success)):
+                    echo "<p class='alert alert-success text-center'>$success</p>";
+                endif;
+                ?>
                 <form method="post" action="<?php url('Product/store') ?>">
                     <div class="mb-3">
                         <label class="form-label" for="product">Name</label>
-                        <input required type="text" class="form-control" id="product" placeholder="Product Name" name="product">
+                        <input required type="text" class="form-control" id="product" placeholder="Product Name"
+                               name="product">
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="product">Price</label>
-                        <input required type="text" class="form-control" id="product" placeholder="Product Price" name="Price">
+                        <input required type="text" class="form-control" id="product" placeholder="Product Price"
+                               name="price">
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="product">Description</label>
-                        <input required type="text" class="form-control" id="product" placeholder="Product Description" name="Description">
+                        <input required type="text" class="form-control" id="product" placeholder="Product Description"
+                               name="description">
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="product">Quantity</label>
-                        <input required type="number" pattern="^[1-9][0-9]*$" class="form-control" id="product" placeholder="Product Quantity" name="Quantity" min="1" required>
+                        <input required type="number" pattern="^[1-9][0-9]*$" class="form-control" id="product"
+                               placeholder="Product Quantity" name="quantity" min="1" required>
                     </div>
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-primary">Add</button>
+                        <button type="submit" name="submit" class="btn btn-primary">Add</button>
                     </div>
                 </form>
             </div>

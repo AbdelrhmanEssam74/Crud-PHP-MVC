a8b862308efe5afbe3305a0a033996ebf758f289
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-01 19:49:02 +0300

    Refactor HomeController to simplify index method by removing unnecessary data assignment.

--- APP/Controllers/HomeController.php
+++ APP/Controllers/HomeController.php
@@ -4,8 +4,6 @@ class HomeController
 {
     public function index($id): void
     {
-        $data["title"] = "Home Page";
-        $data["content"] = "This is the home page content";
-        View::load("home", $data);
+        View::load("home");
     }
 }

08b857080fe1b9395bbc5e718c1e17e031202215
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-01 19:48:28 +0300

    Add header and footer templates with Bootstrap integration

---
+++ APP/Views/includes/templates/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="<?php url(); ?>">
+        <img src="<?php echo APP_URL . 'assets\images\logo.png'; ?>" width="100" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php url(); ?>">Home </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="<?php url('Product/index'); ?>"">Products</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php url('Product/add'); ?>"">Add Product</a>
+            </li>
+
+        </ul>
+
+    </div>
+</nav>
+

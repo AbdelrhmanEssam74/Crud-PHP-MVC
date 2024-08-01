b6b375c791a0e77bf32cb92b39adc1de8e8623e6
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-01 15:47:34 +0300

    Update HomeController to set page title and content, and load the home view

--- APP/Controllers/HomeController.php
+++ APP/Controllers/HomeController.php
@@ -4,6 +4,8 @@ class HomeController
 {
     public function index($id): void
     {
-        echo "Home";
+        $data["title"] = "Home Page";
+        $data["content"] = "This is the home page content";
+        View::load("home", $data);
     }
 }

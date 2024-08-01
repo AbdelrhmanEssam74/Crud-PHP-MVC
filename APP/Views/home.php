9004b32112da1c5019410aa3043c02deeda5cafc
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-01 19:49:17 +0300

    Refactor home.php to use template includes and update content layout
    - Replace static HTML structure with header and footer includes.
    - Add a jumbotron-style component for enhanced content presentation.
    - Update content to better describe the application features.

--- APP/Views/home.php
+++ APP/Views/home.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $title ?></title>
-</head>
-<body>
-    <p><?php echo $content?></p>
-</body>
-</html>
+<?php include Templates . "header.php"; ?>
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3 bg-dark-subtle text-center mt-5">
+        <h1 class="display-4">Simple CURD System</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg " href="<?php url('product') ?>" role="button">SHOW PRODUCTS</a>
+    </div>
+<?php include Templates . "footer.php"; ?>

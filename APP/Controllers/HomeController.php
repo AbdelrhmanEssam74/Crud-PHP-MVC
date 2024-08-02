a6359b8d7e22106e400808fcc74fc744ca2135f4
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-02 19:16:36 +0300

    Add 404 Not Found view method to HomeController

--- APP/Controllers/HomeController.php
+++ APP/Controllers/HomeController.php
@@ -7,4 +7,10 @@ class HomeController
         $data['title'] = "Home";
         View::load("home" , $data);
     }
+    public static function not_found(): void
+    {
+        $data['title'] = "404 Not Found";
+        View::load('Not Found' , $data);
+    }
+
 }

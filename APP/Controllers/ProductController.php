5420b288557e2e98f21a291e72773d45c83efd79
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-02 18:08:03 +0300

    Add product deletion method and improve store functionality in ProductController
    - Implement `delete` method to handle product removal
    - Enhance `store` method with error handling and form data processing
    - Add comments for better code clarity and maintainability

--- APP/Controllers/ProductController.php
+++ APP/Controllers/ProductController.php
@@ -19,24 +19,49 @@ class ProductController
 
     public function store(): void
     {
+        // Check if the form is submitted
         if (isset($_POST['submit'])):
+            // Get form data
             $product_name = $_POST['product'];
             $product_price = $_POST['price'];
             $product_desc = $_POST['description'];
             $product_quantity = $_POST['quantity'];
+
+            // Prepare data array
             $data = array(
                 "name" => $product_name,
                 "price" => $product_price,
                 "description" => $product_desc,
                 "quantity" => $product_quantity
             );
+
             $db = new product(); // Create a new instance of the product model
+
             try {
+                // Insert data into the database
                 $db->insert($data);
                 View::load("Product/add", ['success' => "Product added successfully"]);
             } catch (Exception $e) {
+                // Handle exceptions
                 View::load("Product/add");
             }
         endif;
+    }
+
+    public function delete($id): void
+    {
+        $db = new product(); // Create a new instance of the product model
+        $data['success'] = "Product deleted successfully";
+
+        if ($db->delete($id[2])) {
+            // If product is deleted successfully
+            $data['products'] = $db->getAllProduct(); // Fetch all products from the database
+            $data['title'] = "Products"; // Set the title for the view
+            View::load('Product/index', $data); // Redirect back to the products page after deletion
+            exit; // Stop further execution to prevent loading unnecessary views
+        } else {
+            // If deletion fails
+            echo "Error deleting product";
+        }
     }
 }

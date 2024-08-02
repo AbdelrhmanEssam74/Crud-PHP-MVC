48019ebb9a75c0dc3fd34240e698add3cf09a7fe
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-08-02 18:08:18 +0300

    Remove error message display from product index view

--- APP/Views/Product/index.php
+++ APP/Views/Product/index.php
@@ -6,9 +6,6 @@
                 <?php if(isset($success)): ?>
                     <h3 class="alert alert-success text-center"><?php  echo $success; ?></h3>
                 <?php endif; ?>
-                <?php if(isset($error)): ?>
-                    <h3 class="alert alert-danger text-center"><?php  echo $error; ?></h3>
-                <?php endif; ?>
                 <table class="table table-hover">
                     <thead class="thead-dark">
                     <tr>
